2bd5e22a85f277de33b2d8342d0393bf59753147
Halil Han <halilhanbadem@gmail.com>
2020-09-26 23:21:33 +0300

    Delphi Client commit

---
+++ delphi_api_client/Unit1.dfm
@@ -0,0 +1,50 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 299
+  ClientWidth = 635
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 35
+    Width = 185
+    Height = 25
+    Caption = 'Sorgula'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 8
+    Top = 8
+    Width = 185
+    Height = 21
+    TabOrder = 1
+  end
+  object NetHTTPClient1: TNetHTTPClient
+    Asynchronous = False
+    ConnectionTimeout = 60000
+    ResponseTimeout = 60000
+    HandleRedirects = True
+    AllowCookies = True
+    UserAgent = 'Embarcadero URI Client/1.0'
+    Left = 96
+    Top = 112
+  end
+  object NetHTTPRequest1: TNetHTTPRequest
+    Asynchronous = False
+    ConnectionTimeout = 60000
+    ResponseTimeout = 60000
+    Client = NetHTTPClient1
+    Left = 32
+    Top = 104
+  end
+end
---
+++ delphi_api_client/Unit1.pas
@@ -0,0 +1,51 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.JSON, System.Net.URLClient,
+  System.Net.HttpClient, System.Net.HttpClientComponent, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    NetHTTPClient1: TNetHTTPClient;
+    NetHTTPRequest1: TNetHTTPRequest;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+ url, json_donus: string;
+ param: TStringList;
+ jsonObj: TJSONValue;
+ A: TJSONAncestor;
+begin
+ param := TStringList.Create;
+ param.Add('lisansno='+Edit1.Text);
+ url := 'https://apiservice.wolcod.com/api/lisans';
+ json_donus := NetHTTPRequest1.Post(url, param).ContentAsString();
+ jsonObj := TJSONObject.ParseJSONValue(json_donus);
+ if jsonObj.TryGetValue('lisans_sahibi', jsonObj)  then
+ begin
+   ShowMessage('License Owner: ' + jsonObj.Value);
+ end else
+ begin
+  ShowMessage('License Owner: ' + jsonObj.GetValue<string>('message'));
+ end;
+end;
+
+end.
